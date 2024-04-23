90d2943e2b2c2348f59ecae50295bb0d433e5e91
VoTanTien <115779921+VoTanTien@users.noreply.github.com>
2024-04-23 08:58:13 +0700

    update login and signup ui

---
+++ app/src/main/java/com/example/ecommerce/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.ecommerce;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView register;
+    AppCompatButton main;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        register = (TextView) findViewById(R.id.registerBtn);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        main = (AppCompatButton) findViewById(R.id.buttonLogin);
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/example/ecommerce/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.example.ecommerce;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+public class RegisterActivity extends AppCompatActivity {
+    TextView login;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        login = (TextView) findViewById(R.id.loginBtn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
